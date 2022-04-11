7c0d44dd5436bd59d1ca3893b100e443161aa93f
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-04-11 13:58:16 -0400

    asjadh

--- script.js
+++ script.js
@@ -19,37 +19,6 @@ function button() {
   }
 }
 
-function change() {
-if (getCookie("theme") == "light") {
-  const favicon = document.querySelector('link[rel="icon"]')
-  const button = document.querySelector('.themebutton')
-  const logo = document.querySelector('.logo')
-  favicon.href = "/assets/icon_circle_dark.png"
-  button.src = "/theme_night.png"
-  logo.src = "/assets/icon_dark.png"
-} else if (getCookie("theme") == "dark") {
-  const favicon = document.querySelector('link[rel="icon"]')
-  const button = document.querySelector('.themebutton')
-  const logo = document.querySelector('.logo')
-  favicon.href = "/assets/icon_circle.png"
-  button.src = "/theme_day.png"
-  logo.src = "/assets/icon.png"
-}
-}
-
-function swaptheme() {
-  const currenttheme = getCookie("theme");
-  if (currenttheme == "dark") {
-    setCookie("theme", "light", 365);
-    console.log(currenttheme)
-    change()
-  } else if (currenttheme == "light") {
-    setCookie("theme", "dark", 365);
-    console.log(currenttheme)
-    change()
-  }
-}
-
 /*/ irrelevant but this logs the ID of the key you pressed
 document.addEventListener('keydown', function(key) {
   console.log(key.which)

254a500b81433d89f9ba3316fb6b223980a41b17
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-04-13 14:41:22 -0400

    there

---
+++ works/template/script.js
@@ -0,0 +1,42 @@
+var footer = document.getElementById('footer');
+var popup = document.getElementById('footerpopup');
+var close = document.getElementsByClassName('close')[0];
+
+footer.onclick = function() {
+  popup.style.display = "block";
+}
+
+
+close.onclick = function() {
+  popup.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+}
+}
+
+function change() {
+  const favicon = document.querySelector('link[rel="icon"]');
+  const box = document.getElementById('box');
+  const footer = document.getElementById('footer');
+  const popup = document.getElementById('popup-content');
+  if (getCookie("theme") == "light") {
+    favicon.href = "/assets/icon_circle_dark.png";
+    box.style.backgroundColor = "rgba(50, 51, 48, 0.4)";
+    box.style.color = "white"
+    footer.style.backgroundColor = "rgba(50, 51, 48, 0.4)";
+    footer.style.color = "white"
+    popup.style.backgroundColor = "rgba(50, 51, 48, 0.8)"
+    popup.style.color = "white"
+  } else if (getCookie("theme") == "dark") {
+    favicon.href = "/assets/icon_circle.png";
+    box.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+    box.style.color = "black"
+    footer.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+    footer.style.color = "black"
+    popup.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    popup.style.color = "black"
+  }
+  }

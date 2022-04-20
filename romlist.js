ce8b5ddc3073dcd27bd5c577b7dc7db50e566cec
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-04-20 10:19:38 -0400

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -5,4 +5,6 @@ var ROMLIST = [
     {url:"roms/doom64.z64",title:"Doom 64"},
     {url:"roms/007.z64",title:"Goldeneye 007"},
     {url:"roms/mariokart.z64",title:"Mario Kart 64"},
+    {url:"roms/scoobydoo.z64",title:"Scooby-Doo! Classic Creep Capers"},
+    {url:"roms/ssb.z64",title:"Super Smash Bros."},
 ];

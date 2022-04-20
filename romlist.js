512d847ad987a6e47225ea556df089d594a95b11
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-04-20 10:14:38 -0400

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -1,5 +1,8 @@
 var ROMLIST = [
     {url:"roms/sm64.z64",title:"Super Mario 64"},
     {url:"roms/starfox.z64",title:"Star Fox 64"},
-    {url:"roms/zeldaoot.z64",title:"Zelda: Ocarina of Time"},
+    {url:"roms/banjokazooie.z64",title:"Banjo-Kazooie"},
+    {url:"roms/doom64.z64",title:"Doom 64"},
+    {url:"roms/007.z64",title:"Goldeneye 007"},
+    {url:"roms/mariokart.z64",title:"Mario Kart 64"},
 ];

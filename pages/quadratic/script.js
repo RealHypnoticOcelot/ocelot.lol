5b93d058c52612ab9787c683a27e0de96c110ff2
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-10 13:57:43 -0400

    the when

--- pages/quadratic/script.js
+++ pages/quadratic/script.js
@@ -1,17 +1,32 @@
-function gcd(x, y) {
-    if (y === 0) return x;
-    /* else */ return gcd(y, x % y);
-}
 function get() {
-    var a = document.getElementById('a').value;
-    var b = document.getElementById('b').value;
-    document.getElementById('output').innerHTML = gcd(a, b);
+    const num1 = +document.querySelector('#a').value;
+    const num2 = +document.querySelector('#b').value;
+    const num3 = +document.querySelector('#c').value;
+    const output = document.querySelector('#output');
+    const multTarget = num1 * num3;
+    const addTarget = num2;
+
+    const absMult = Math.abs(multTarget);
+
+for (let num1 = -absMult; num1 <= absMult; num1++) {
+
+  for (let num2 = -absMult; num2 <= absMult; num2++) {
+    
+    if (
+      (num1 * num2 === multTarget) 
+      && (num1 + num2 === addTarget)
+    ) {
+      output.innerHTML = ([ num1 + ", " + num2 ]);
+    }
+
+  }
 }
+}
+
 // Get the input field
 var inputa = document.getElementById("a");
 var inputb = document.getElementById("b");
-
-// Execute a function when the user releases a key on the keyboard
+var inputc = document.getElementById("c");
 inputa.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
@@ -19,7 +34,6 @@ inputa.addEventListener("keyup", function(event) {
     get()
   }
 });
-
 inputb.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
@@ -27,3 +41,19 @@ inputb.addEventListener("keyup", function(event) {
     get()
   }
 });
+inputc.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    get()
+  }
+});
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+	
+    document.querySelector('#a').value = '';
+    document.querySelector('#b').value = '';
+    document.querySelector('#c').value = '';
+    document.querySelector('#output').innerHTML = '';
+  });

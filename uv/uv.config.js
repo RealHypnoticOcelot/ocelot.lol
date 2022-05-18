9b08b94585432064b24dc88eb69d9dfe6dda489f
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-17 22:43:58 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://nodejs-bare-server-node.realhypnoticoce.repl.co',
+    bare: 'https://nodejs-bare-server-node.realhypnoticoce.repl.co/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

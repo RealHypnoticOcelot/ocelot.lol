6b93a13bead9cadb339d716b1ea8b3a37a838d7f
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-17 21:38:24 -0400

    add more buttons

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ function proxy_geforce() {
     window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 }
 
+function proxy_now() { 
+    let url = "now.gg/lp/scopely/1280/looney-tunes";
+    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
+
+function proxy_discord() { 
+    let url = "discord.com/app";
+    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
+
 function isUrl(val = ''){
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;

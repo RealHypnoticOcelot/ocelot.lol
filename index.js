7cea70d077ccb8982e96765af9bdef2520220b87
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-17 21:27:44 -0400

    js

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ form.addEventListener('submit', async event => {
     });
 });
 
+function proxy_geforce() { 
+    let url = "play.geforcenow.com";
+    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
+
 function isUrl(val = ''){
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;

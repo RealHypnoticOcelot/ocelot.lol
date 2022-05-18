395496fb656406042074b3260885d59dc94e9374
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-18 15:29:36 -0400

    fix

--- index.js
+++ index.js
@@ -16,14 +16,10 @@ form.addEventListener('submit', async event => {
 });
 
 function proxy_geforce() { 
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(); {
     let url = "play.geforcenow.com";
     if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
     else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
     window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    }
 }
 
 function proxy_now() { 

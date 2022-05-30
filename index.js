ab08ca649e3f3b3a0c5175faac4e8eb2854c128c
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-05-30 19:43:04 -0400

    remove deprecated functions

--- index.js
+++ index.js
@@ -15,41 +15,6 @@ form.addEventListener('submit', async event => {
     });
 });
 
-function proxy_geforce() { 
-    let url = "play.geforcenow.com";
-    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-}
-
-function proxy_now() { 
-    let url = "now.gg/lp/scopely/1280/looney-tunes";
-    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-}
-
-function proxy_discord() { 
-    let url = "discord.com/app";
-    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-}
-
-function proxy_yt() { 
-    let url = "youtube.com";
-    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-}
-
-function proxy_spotify() { 
-    let url = "open.spotify.com";
-    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-}
-
 function proxy_btn() {
     let link =  event.target.getAttribute('link')
     const form = document.querySelector('#forminput')

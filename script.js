0a6ab36aabe81fa367c7c47c42b680f376d322c6
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-07-08 15:33:25 -0400

    uhf

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function button() {
 function clipboard() {
   let clip = event.target.getAttribute('clip')
   console.log(clip)
-  navigator.clipboard.writeText(clip.value);
+  navigator.clipboard.writeText(clip);
   alert("Copied to clipboard! (" + clip + ")")
 }
 
